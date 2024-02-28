1acb186bdeaaab60219295759d011b8d45f0feca
benup211 <benup211@gmail.com>
2024-02-28 22:52:40 +0545

    added tailwind,daisyui and navbar component

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+import daisyui from "./node_modules/daisyui"
+module.exports = {
+  content: ["./src/**/*.{html,jsx,tsx,js,ts,css,scss,svelte,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
