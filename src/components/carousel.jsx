9c67dca94ac433e817b93194b7654dc38aca9536
benup211 <benup211@gmail.com>
2024-03-25 21:22:02 +0545

    Changes:Carousel sliding function

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,25 +1,35 @@
 export const Carousel = () => {
     return (
         <>
-            <div className="carousel w-full max-h-96 min-h-48">
-                <div id="item1" className="carousel-item w-full">
-                    <img src="https://www.theitdepot.com/assets/images/banners/index_65df235aab90f.png" className="w-full h-auto object-contain" />
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://www.theitdepot.com/assets/images/banners/index_65df235aab90f.png" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-                <div id="item2" className="carousel-item w-full">
-                    <img src="https://promotions.newegg.com/nepro/24-0215/1920x660.jpg" className="w-full h-auto object-contain" />
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://promotions.newegg.com/nepro/24-0215/1920x660.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-                <div id="item3" className="carousel-item w-full">
-                    <img src="https://www.theitdepot.com/images/headerScroll_new/gaming%20x%20slim.png" className="w-full h-auto object-contain" />
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://www.theitdepot.com/images/headerScroll_new/gaming%20x%20slim.png" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-                <div id="item4" className="carousel-item w-full">
-                    <img src="https://www.theitdepot.com/images/headerScroll_new/mother%20board.png" className="w-full h-auto object-contain"/>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src="https://www.theitdepot.com/images/headerScroll_new/mother%20board.png" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
                 </div>
-            </div>
-            <div className="flex justify-center w-full py-2 gap-2">
-                <a href="#item1" className="btn btn-xs">1</a>
-                <a href="#item2" className="btn btn-xs">2</a>
-                <a href="#item3" className="btn btn-xs">3</a>
-                <a href="#item4" className="btn btn-xs">4</a>
             </div>
         </>
     )
