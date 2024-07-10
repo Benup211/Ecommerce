1b0673c2dc7c6793c78f2287f1f8d4fbbd65b9d5
benup211 <benup211@gmail.com>
2024-07-10 22:47:51 +0545

    esewa integration

--- frontend/src/components/cartdetail.jsx
+++ frontend/src/components/cartdetail.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import { ToastContainer,toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
+import { v4 as uuidv4 } from 'uuid';
+import * as CryptoJS from 'crypto-js';
 
 function ShoppingCart() {
   const [cartItems, setCartItems] = useState({});
   const [subtotal, setSubtotal] = useState(0);
-  const [transactionUUID, setTransactionUUID] = useState('');
+  const [transactionUUID, setTransactionUUID] = useState(uuidv4());
   const shippingCost = 500;
   const taxRate = 0.13;
 
@@ -60,7 +62,12 @@ function ShoppingCart() {
     localStorage.removeItem('cart');
     const redirectUrl = `https://uat.esewa.com.np/api/epay/transaction/status/?product_code=EPAYTEST&total_amount=${total}&transaction_uuid=123`;
     window.location.href = redirectUrl;
-  };  
+  };
+  const t=100;
+  const message = `total_amount=${subtotal},transaction_uuid=${transactionUUID},product_code=EPAYTEST`;
+  console.log(message)
+  const hash = CryptoJS.HmacSHA256(message, "8gBm/:&EnhH.1/q");
+  const hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
   return (
     <section className="h-100 h-custom">
       <div className="container py-5 h-100">
@@ -80,20 +87,20 @@ function ShoppingCart() {
                       {Object.keys(cartItems).map(itemId => (
                         <div key={itemId} className="row mb-4 d-flex justify-content-between align-items-center">
                           <div className="col-md-3 col-lg-3 col-xl-3">
-                          <a href={`/product/${cartItems[itemId].id}`} className="text-black mb-0">{cartItems[itemId].name}</a>
+                            <a href={`/product/${cartItems[itemId].id}`} className="text-black mb-0">{cartItems[itemId].name}</a>
                           </div>
                           <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
-                            <button 
+                            <button
                               className="btn btn-danger px-2"
                               onClick={() => decreaseQuantity(itemId)}
                             >-</button>
-                            <input 
+                            <input
                               type="number"
                               className="form-control form-control-sm"
                               value={cartItems[itemId].order}
                               readOnly
                             />
-                            <button 
+                            <button
                               className="btn btn-danger px-2"
                               onClick={() => increaseQuantity(itemId)}
                             >+</button>
@@ -102,11 +109,11 @@ function ShoppingCart() {
                             <h6 className="mb-0">Rs {cartItems[itemId].price * cartItems[itemId].order}</h6>
                           </div>
                           <div className="col-md-1 col-lg-1 col-xl-1 text-end">
-                            <button 
+                            <button
                               className="btn text-muted"
                               onClick={() => handleDeleteItem(itemId)}
                             >
-                              <FontAwesomeIcon icon={faTrash} className="me-2" style={{color: "#f93434",}} />
+                              <FontAwesomeIcon icon={faTrash} className="me-2" style={{ color: "#f93434", }} />
                             </button>
                           </div>
                         </div>
@@ -136,7 +143,32 @@ function ShoppingCart() {
                         <h5 className="text-uppercase">Total price</h5>
                         <h5>Rs {total.toFixed(2)}</h5>
                       </div>
-                      <button type="button" className="btn btn-success btn-block btn-lg" onClick={handleDeleteAllItems} data-mdb-button-init data-mdb-ripple-init data-mdb-ripple-color="dark">Esewa Payment</button>
+                      <form action="https://rc-epay.esewa.com.np/api/epay/main/v2/form" method="POST">
+                        <input type="hidden" id="amount" name="amount" value={`${subtotal}`} required />
+                        <input type="hidden" id="tax_amount" name="tax_amount" defaultValue="0" required />
+                        <input type="hidden" id="total_amount" name="total_amount" value={`${subtotal}`} required />
+                        <input type="hidden" id="transaction_uuid" name="transaction_uuid" value={`${transactionUUID}`} required />
+                        <input type="hidden" id="product_code" name="product_code" defaultValue="EPAYTEST" required />
+                        <input type="hidden" id="product_service_charge" name="product_service_charge" defaultValue="0" required />
+                        <input type="hidden" id="product_delivery_charge" name="product_delivery_charge" defaultValue="0" required />
+                        <input type="hidden" id="success_url" name="success_url" defaultValue="http://localhost:5173/cart" required />
+                        <input type="hidden" id="failure_url" name="failure_url" defaultValue="https://google.com" required />
+                        <input type="hidden" id="signed_field_names" name="signed_field_names" defaultValue="total_amount,transaction_uuid,product_code" required />
+                        <input type="hidden" id="signature" name="signature" value={`${hashInBase64}`} required />
+                        <button
+                          type="submit"
+                          style={{
+                            display: 'block !important',
+                            backgroundColor: '#60bb46',
+                            cursor: 'pointer',
+                            color: '#fff',
+                            border: 'none',
+                            padding: '5px 10px',
+                          }}
+                        >
+                          Esewa Payment
+                        </button>
+                      </form>
                     </div>
                   </div>
                 </div>
@@ -145,7 +177,7 @@ function ShoppingCart() {
           </div>
         </div>
       </div>
-      <ToastContainer/>
+      <ToastContainer />
     </section>
   );
 }
